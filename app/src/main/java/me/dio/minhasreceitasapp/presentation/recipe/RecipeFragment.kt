02e6ba1885602fe50368cb3bd51ff01a9fa042aa
igor.bagliotti <igorbag@hotmail.com>
2022-11-01 08:36:50 -0300

    Make first layout to recipe

--- app/src/main/java/me/dio/minhasreceitasapp/presentation/recipe/RecipeFragment.kt
+++ app/src/main/java/me/dio/minhasreceitasapp/presentation/recipe/RecipeFragment.kt
@@ -1,10 +1,10 @@
 package me.dio.minhasreceitasapp.presentation.recipe
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import me.dio.minhasreceitasapp.R
 import me.dio.minhasreceitasapp.databinding.FragmentFirstBinding
@@ -14,18 +14,13 @@ import me.dio.minhasreceitasapp.databinding.FragmentFirstBinding
  */
 class RecipeFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentFirstBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+    ): View {
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
 
     }
